35f5b51e443c36abeeb3cef1fe33ae61b7dcfc75
Morgan OLeary <rmhwow@gmail.com>
2015-07-27 14:35:11 -0400

    have the correct Task/User association

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150727174432) do
+ActiveRecord::Schema.define(version: 20150727175224) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+  # Create Users
+ 5.times do
+   user = User.new(
+     name:     Faker::Name.name,
+     email:    Faker::Internet.email,
+     password: Faker::Lorem.characters(10)
+   )
+   user.save!
+   user.update_attributes!(created_at: rand(10.minutes .. 1.year).ago)
+ end
+
+admin = User.new(
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: 'helloworld',
+    )
+  admin.save!
+ users = User.all
+
+50.times do 
+  task = Task.create!(
+    body: Faker::Lorem.sentence,
+    user: users.sample
+    )
+   task.update_attributes!(created_at: rand(10.minutes .. 1.year).ago)
+   task.save!
+end
+
+tasks = Task.all
