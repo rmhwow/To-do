46fae4c113d5c6d54a738789899e1292c5c4f854
Morgan OLeary <rmhwow@gmail.com>
2015-07-28 15:23:42 -0400

    validates email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   has_many :tasks
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+validates :email, presence: true, uniqueness: :true, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
 end
